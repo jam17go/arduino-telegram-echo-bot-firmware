56245e5b6f0c02bec66b1536bb1e8190b7456bc1
Konstantin Dudinov <Konstantin.Dudinov@genesys.com>
2021-11-15 16:19:34 +0300

    Update serial output

--- arduino-matrix-http-client.ino
+++ arduino-matrix-http-client.ino
@@ -24,7 +24,7 @@ const char kPath[] = "/";
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 const int kNetworkTimeout = 30*1000;
-const int kNetworkDelay = 1000;
+const int kNetworkDelay = 10000;
 
 IPAddress dnServer(192, 168, 88, 1);
 IPAddress gateway(192, 168, 88, 1);
@@ -39,6 +39,7 @@ void setup()
   Serial.begin(9600); 
   
   Parola.begin();
+
   Ethernet.begin(mac, ip, dnServer, gateway, subnet);
 
   Parola.displayText("Ethernet ok", PA_CENTER, 25, 2000, PA_SCROLL_DOWN, PA_SCROLL_UP);
@@ -48,9 +49,7 @@ void setup()
 }
 
 void loop()
-{
-  //Ethernet.maintain();
-  
+{  
   char data[512];
   int pos = 0;
   memset(&data, 0, sizeof(data));
@@ -68,10 +67,6 @@ void loop()
       Serial.print("Got status code: ");
       Serial.println(err);
 
-      // Usually you'd check that the response code is 200 or a
-      // similar "success" code (200-299) before carrying on,
-      // but we'll print out whatever response we get
-
       err = http.skipResponseHeaders();
       if (err >= 0)
       {
@@ -81,28 +76,24 @@ void loop()
         Serial.println();
         Serial.println("Body returned follows:");
       
-        // Now we've got to the body, so we can print it out
         unsigned long timeoutStart = millis();
         char c;
-        // Whilst we haven't timed out & haven't reached the end of the body
+
         while ( (http.connected() || http.available()) &&
                ((millis() - timeoutStart) < kNetworkTimeout) )
         {
             if (http.available())
             {
                 c = http.read();
-                // Print out this character
                 data[pos++] = c;
+                
                 Serial.print(c);
-               
+
                 bodyLen--;
-                // We read something, reset the timeout counter
                 timeoutStart = millis();
             }
             else
             {
-                // We haven't got any data, so let's pause to allow some to
-                // arrive
                 delay(kNetworkDelay);
             }
         }
@@ -121,21 +112,15 @@ void loop()
   }
   else
   {
-    Parola.displayText("Connect failed", PA_CENTER, 25, 2000, PA_SCROLL_DOWN, PA_SCROLL_UP);
-    while (Parola.displayAnimate() == false) {
-      Parola.displayAnimate();
-    }
-    //Serial.print("Connect failed: ");
-    //Serial.println(err);
+    Serial.print("Connect failed: ");
+    Serial.println(err);
   }
+  
   http.stop();
-
-  // And just stop, now that we've tried a download
-
+  
   Parola.displayText(data, PA_LEFT, 25, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
   while (Parola.displayAnimate() == false) {
     Parola.displayAnimate();
   }
-  
   delay(3000);
 }
